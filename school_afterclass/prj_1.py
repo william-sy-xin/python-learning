6acd7b3ee02453112be3a39a5225b0e86b3b2eaf
William Xin <william.xin.siyuan@gmail.com>
2025-08-05 11:34:31 -0700

    added school_afterclass/

---
+++ school_afterclass/prj_1.py
@@ -0,0 +1,11 @@
+x=14
+y=14
+if 9*9==81 or x*y==196:
+  print("True")
+
+  x=14
+  y=14
+  
+if 9*9==99 or x*y==197:
+      print("False")
+
---
+++ school_afterclass/prj_2_GreedyCanineTrueSpace.py
@@ -0,0 +1,13 @@
+grade = int(input("What is your grade? "))
+if grade >= 100:
+  print("You have an A+")
+elif grade >= 90:
+  print("you got an A")
+elif grade >= 80:
+  print("you got an B")
+elif grade >= 70:
+  print("you got an C")
+elif grade >= 60:
+  print("you got an D")
+else:
+  print ("you got a F")
